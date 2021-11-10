9505086cc26d14fd80ce0ffb8f59b01e35955d47
Muniz890 <91911992+Muniz890@users.noreply.github.com>
2021-11-09 23:01:28 -0300

    entrega: katas2 js

--- src/katas2.js
+++ src/katas2.js
@@ -1,36 +1,78 @@
 // comece a criar a sua função add na linha abaixo
 
-// descomente a linha seguinte para testar sua função
-// console.assert(add(3, 5) === 8, 'A função add não está funcionando como esperado');
-
-
-// comece a criar a sua função multiply na linha abaixo
-
-
-// descomente a linha seguinte para testar sua função
-// console.assert(multiply(4, 6) === 24, 'A função multiply não está funcionando como esperado');
-
-
-// comece a criar a sua função power na linha abaixo
-
-
-// descomente a linha seguinte para testar sua função
-// console.assert(power(3, 4) === 81, 'A função power não está funcionando como esperado');
-
-
-// comece a criar a sua função factorial na linha abaixo
-
-
-// descomente a linha seguinte para testar sua função
-// console.assert(factorial(5) === 120, 'A função factorial não está funcionando como esperado');
-
-
-/**
- * BONUS (aviso: o grau de dificuldade é bem maior !!!)
- */
-
-// crie a função fibonacci
-
-
-// descomente a linha seguinte para testar sua função
-// console.assert(fibonacci(8) === 13, 'A função fibonacci não está funcionando como esperado');
+function add(a,b){
+    let resultadoAdd = a + b;
+    return resultadoAdd;
+  }
+  add(3,4)
+  
+  // descomente a linha seguinte para testar sua função
+  
+  //console.assert(add(3, 5) === 8;
+  
+  
+  // comece a criar a sua função multiply na linha abaixo
+  
+  function multiply(a,b){
+    let resultadotMult = 0
+    for(let i = 0; i<b; i++)
+       resultadotMult += add(a, 0);
+    return resultadotMult;
+  }
+  multiply(8,12)
+  
+  // descomente a linha seguinte para testar sua função
+  //console.assert(multiply(4, 6) === 24;
+  
+  
+  // comece a criar a sua função power na linha abaixo
+  
+  function pow(x,n){
+    let resultadoPow = 1;
+    for(let i = 1; i<=n; i++)
+      resultadoPow *= multiply(x,x)/x;
+    return  resultadoPow
+  }
+  pow(3,4)
+  
+  // descomente a linha seguinte para testar sua função
+  //console.assert(power(3, 4) === 81;
+  
+  
+  // comece a criar a sua função factorial na linha abaixo
+  
+  function factorial(a){
+    let resultadoFact = 1;
+    for(let i = 1; i<=a; i++)
+      resultadoFact *= multiply(i, 1);
+    return resultadoFact;  
+  }
+  factorial(6)
+  
+  // descomente a linha seguinte para testar sua função
+  // console.assert(factorial(5) === 120, 'A função factorial não está funcionando como esperado');
+  
+  
+  /**
+   * BONUS (aviso: o grau de dificuldade é bem maior !!!)
+   */
+  
+  // crie a função fibonacci
+  
+  function fibonacci(n){
+    let arr = [];
+    for(let i = 0; i<=n; i++){
+      if(i === 0){
+        arr.push(0);
+      } else if (i === 1) {
+        arr.push(1);
+      } else {
+        arr.push(add(arr[i-1],arr[i-2]))
+      }
+    }
+    return arr[n-1];
+  }
+  
+  // descomente a linha seguinte para testar sua função
+  // console.assert(fibonacci(8) === 13, 'A função fibonacci não está funcionando como esperado');
+  
